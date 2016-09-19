8b89e98689f897ac5c6477c49ac12ebbe12dd3f1
Alovinglol <Ilegend2011@gmail.com>
2016-09-19 17:45:25 +0300

    Допиливаю,осталось половина второго задания

--- js/pictures.js
+++ js/pictures.js
@@ -1,19 +1,34 @@
-window.onload = function(){
-  var loadedData = null;
-  var container = document.querySelector('.pictures');
 
+
+  var container = document.querySelector('.pictures');
   var filters = document.querySelectorAll('.filters-radio');
   var activeFilter = 'filter-popular';
   var pictures = [];
   var currentPage = 0;
   var PAGE_SIZE = 12;
+  var footer = document.querySelector('.footer');
+  var footerHeight = footer.getBoundingClientRect().height;
+  var visibleFooter  = function (){
+    return footer.getBoundingClientRect().bottom - window.innerHeight <= footerHeight
+  }
+  getPictures();
+
+
   var filteredPictures = [];
-  for(var i = 0; i < filters.length; i++){
-    filters[i].onclick = function(evt){
-      var clickedElementID = evt.target.id;
-      setActiveFilter(clickedElementID);
-    };
-  };
+
+
+  document.querySelector('.filters').addEventListener('click',function(evt){
+    var clickeElement = evt.target;
+    if(clickeElement.classList.contains('filters-radio')){
+      setActiveFilter(clickeElement.id);
+    }
+  });
+  // for(var i = 0; i < filters.length; i++){
+  //   filters[i].onclick = function(evt){
+  //     var clickedElementID = evt.target.id;
+  //     setActiveFilter(clickedElementID);
+  //   };
+  // };
 
 
   function setActiveFilter(id){
@@ -24,38 +39,39 @@ window.onload = function(){
     switch(id){
       case 'filter-popular':
         filteredPictures = pictures.slice(0);
+        currentPage = 0;
       break;
       case 'filter-new':
         filterePictures = pictures.slice(0);
         filteredPictures = filteredPictures.sort(function(a,b){
           return Date.parse(a.date) - Date.parse(b.date);
         });
+        currentPage = 0;
       break;
       case 'filter-discussed':
         filterePictures = pictures.slice(0);
         filteredPictures = filteredPictures.sort(function(a,b){
           return b.comments - a.comments;
         });
+        currentPage = 0;
       break;
     }
     activeFilter = id;
     renderPictures(filteredPictures,0,true);
   };
-  window.addEventListener('scroll',function(env){
-    var footerCoordinates = document.querySelector('.footer').getBoundingClientRect();
-    var viewportSize = window.innerHeight;
 
-    if(footerCoordinates.bottom - window.innerHeight <= footerCoordinates.height){
-      renderPictures(filteredPictures,currentPage++);
+
+
+  window.addEventListener('scroll', function(env){
+    if(visibleFooter()){
+      renderPictures(pictures,currentPage++);
     }
-  });
-  getPictures();
-
+  })
 
   function renderPictures(pictures, pageNumber, replace){
     if(replace){
       container.innerHTML = '';
-    }
+    };
 
     var from = pageNumber * PAGE_SIZE;
     var to = from + PAGE_SIZE;
@@ -67,8 +83,11 @@ window.onload = function(){
       fragment.appendChild(element);
     });
     container.appendChild(fragment);
-  };
-   var
+
+    if(visibleFooter()){
+      renderPictures(pictures,currentPage++);
+    }
+  }
   function getPictures(){
     var xhr = new XMLHttpRequest();
     xhr.open('GET','data/pictures.json');
@@ -76,9 +95,8 @@ window.onload = function(){
       var rawData = evt.target.response;
       var picturesJSON = JSON.parse(rawData);
       pictures = picturesJSON;
-      renderPictures(picturesJSON,0)
-    }
-    console.log(pictures);
+      renderPictures(pictures,currentPage);
+    };
     xhr.send()
   }
   function templateFunction(item){
@@ -106,4 +124,3 @@ window.onload = function(){
           }, IMAGE_LOAD_TIMEOUT);
     return element;
   };
-}
--- js/upload.js
+++ js/upload.js
@@ -10,7 +10,7 @@
   /** @enum {string} */
 
 
-/*Функция добавления кукисов 
+/*Функция добавления кукисов
 **name,value options {}
 */
 
@@ -112,7 +112,7 @@
   function resizeFormIsValid() {
     if (!xInput.value || !sizeInput.value || !yInput.value) {
       return false;
-    }else if (currentResizer._image.naturalWidth >= +xInput.value + +sizeInput.value && 
+    }else if (currentResizer._image.naturalWidth >= +xInput.value + +sizeInput.value &&
       currentResizer._image.naturalHeight >= +yInput.value + +sizeInput.value  ) {
       return true;
     }else {
@@ -131,7 +131,7 @@
       resizeBtn.disabled = true;
     }
   }
-  
+
   /**
    * Задает фильтр по умолчанию из cookie.
    */
@@ -186,7 +186,7 @@ function getCookie(name) {
   var yInput = resizeForm.elements.y;
   var sizeInput = resizeForm.elements.size;
 
-  
+
   /**
    * Форма добавления фильтра.
    * @type {HTMLFormElement}
@@ -239,7 +239,7 @@ function getCookie(name) {
    * и показывается форма кадрирования.
    * @param {Event} evt
    */
-  uploadForm.onchange = function(evt) {
+  uploadForm.addEventListener('change',function(evt) {
     var element = evt.target;
     if (element.id === 'upload-file') {
       // Проверка типа загружаемого файла, тип должен быть изображением
@@ -269,12 +269,12 @@ function getCookie(name) {
         showMessage(Action.ERROR);
       }
     }
-  };
+  });
   /**
    * Обработка ввода значений в поле формы кадрирования.
    */
 
-  resizeForm.oninput = function() {
+  resizeForm.addEventListener('submit',function() {
     var maxInputX = currentResizer._image.naturalWidth - sizeInput.value;
     var maxInputY = currentResizer._image.naturalHeight - sizeInput.value;
 
@@ -291,13 +291,13 @@ function getCookie(name) {
     }
 
     toggleFormSubmit();
-  };
+  });
   /**
    * Обработка сброса формы кадрирования. Возвращает в начальное состояние
    * и обновляет фон.
    * @param {Event} evt
    */
-  resizeForm.onreset = function(evt) {
+  resizeForm.addEventListener('reset', function(evt) {
     evt.preventDefault();
 
     cleanupResizer();
@@ -305,14 +305,14 @@ function getCookie(name) {
 
     resizeForm.classList.add('invisible');
     uploadForm.classList.remove('invisible');
-  };
+  });
 
   /**
    * Обработка отправки формы кадрирования. Если форма валидна, экспортирует
    * кропнутое изображение в форму добавления фильтра и показывает ее.
    * @param {Event} evt
    */
-  resizeForm.onsubmit = function(evt) {
+  resizeForm.addEventListener('submit', function(evt) {
     evt.preventDefault();
 
     if (resizeFormIsValid()) {
@@ -321,25 +321,25 @@ function getCookie(name) {
       resizeForm.classList.add('invisible');
       filterForm.classList.remove('invisible');
     }
-  };
+  });
 
   /**
    * Сброс формы фильтра. Показывает форму кадрирования.
    * @param {Event} evt
    */
-  filterForm.onreset = function(evt) {
+  filterForm.addEventListener('reset', function(evt) {
     evt.preventDefault();
 
     filterForm.classList.add('invisible');
     resizeForm.classList.remove('invisible');
-  };
+  });
 
   /**
    * Отправка формы фильтра. Возвращает в начальное состояние, предварительно
    * записав сохраненный фильтр в cookie.
    * @param {Event} evt
    */
-  filterForm.onsubmit = function(evt) {
+  filterForm.addEventListener('submit', function(evt) {
     evt.preventDefault();
 
     var radio = filterForm.querySelectorAll('[type=radio]');
@@ -374,13 +374,13 @@ function getCookie(name) {
 
     filterForm.classList.add('invisible');
     uploadForm.classList.remove('invisible');
-  };
+  });
 
   /**
    * Обработчик изменения фильтра. Добавляет класс из filterMap соответствующий
    * выбранному значению в форме.
    */
-  filterForm.onchange = function() {
+  filterForm.addEventListener('change', function() {
     if (!filterMap) {
       // Ленивая инициализация. Объект не создается до тех пор, пока
       // не понадобится прочитать его в первый раз, а после этого запоминается
@@ -400,7 +400,7 @@ function getCookie(name) {
     // убрать предыдущий примененный класс. Для этого нужно или запоминать его
     // состояние или просто перезаписывать.
     filterImage.className = 'filter-image-preview ' + filterMap[selectedFilter];
-  };
+  });
 
   cleanupResizer();
   updateBackground();
